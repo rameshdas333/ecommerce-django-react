6c7cd42fd9d2e63b0998234a546406a4d6416c20
Ramesh Das <rameshdascse@gmail.com>
2026-09-13 21:47:39 +0600

    commit data update staticurl

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -177,4 +177,5 @@ CORS_ALLOWED_ORIGINS = [
 STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
