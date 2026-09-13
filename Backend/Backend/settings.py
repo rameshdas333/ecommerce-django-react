1059b2c5ce848dc617490382de81afc114b156c9
Ramesh Das <rameshdascse@gmail.com>
2026-09-13 19:19:34 +0600

    postgresupdate

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-jvwn+s7e+5q1ossk85n2)*@-2dt$92oh+5k^-)rwbybxt42r^5
 DEBUG = False
 # render e host korer jonno change kora hoise
 ALLOWED_HOSTS = [
-     "ecommerce-django-react-9.onrender.com",
+     "ecommerce-django-react-1-c6ro.onrender.com",
     "localhost",
     "127.0.0.1",
     "ecommerce-django-react-gules.vercel.app"
