beb548b238ddb83ccb9373efc05b53142a02a3aa
Ramesh Das <rameshdascse@gmail.com>
2026-09-13 17:24:56 +0600

    Fix Render PostgreSQL database configuration

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -113,15 +113,14 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 
 # render > environment DATABASE_URL= renver internal database url (postgresdatabase render)
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
